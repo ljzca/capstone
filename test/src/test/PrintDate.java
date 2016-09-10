6bf6c731c2e5fded71e62445ffa7156ffd54f911
689626 <689626@ELF5IT212702.ACDM.DS.SAIT.CA>
2016-09-10 16:31:52 -0600

    test

---
+++ test/src/test/PrintDate.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.util.Date;
+
+public class PrintDate {
+
+    public void printDate() {
+        final Date date = new Date();
+        System.out.println(date);
+    }
+}
