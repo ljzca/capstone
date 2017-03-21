babf20068ba8747198fefe1c633d9315616d2be5
ljzca <lijianzhao@lijianzhao.com>
2017-03-21 17:34:29 -0600

    Fix signup

--- src/main/resources/static/js/controllers/signup.js
+++ src/main/resources/static/js/controllers/signup.js
@@ -1,28 +1,30 @@
 angular.module('stars')
 
-.controller("signupCtrl",["$http","$scope","$cookieStore","$location","sendRequest", function($http, $scope, $cookieStore, $location, sendRequest){
-	
-	$scope.register = function(){
+    .controller("signupCtrl", ["$http", "$scope", "$cookieStore", "$location", "sendRequest", function ($http, $scope, $cookieStore, $location, sendRequest) {
 
-		
-		console.log("Button Clicked!");
-		sendRequest.send(
-			'POST',
-			'users',
-			{
-				username: $scope.username,
-				password: $scope.password,
-				email: $scope.email
-			},
-			function (result) {
-				$location.path("signedup");
-            },
-			function (error) {
-				if(error.status === 406){
-					$scope.errMsg = error.data;
-				}
-            },
-            null,
-            null
-		)};
-}]);
+        $scope.register = function () {
+
+
+            console.log("Button Clicked!");
+            sendRequest.send(
+                'POST',
+                'users',
+
+                function (result) {
+                    $location.path("signedup");
+                },
+                function (error) {
+                    if (error.status === 406) {
+                        $scope.errMsg = error.data;
+                    }
+                },
+                {
+                    username: $scope.username,
+                    password: $scope.password,
+                    email: $scope.email
+                },
+                null,
+                null
+            );
+        };
+    }]);
--- src/main/resources/static/js/services/rest.js
+++ src/main/resources/static/js/services/rest.js
@@ -1,31 +1,31 @@
 angular.module('stars')
 
-.service("sendRequest",['$base64', '$http', 'constants', function ($base64, $http, constants) {
+    .service("sendRequest", ['$base64', '$http', 'constants', function ($base64, $http, constants) {
 
-	this.send = function (method, url, sucess, fail, data, username, password) {
-		
-		if(username === null || password === null) {
-			var req = {
-					method: method,
-					url: constants.rootURL + url,
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					data: data
-			};
-		} else {
-			var req = {
-					method: method,
-					url: constants.rootURL + url,
-					headers: {
-						'Content-Type': 'application/json',
-						'Authorization': "Basic " + $base64.encode(username + ':' + password),
-					},
-					data: data,
-					username: username,
-					password: password
-			};		
-		}
-		$http(req).then(sucess, fail);
-    };
-}]);
+        this.send = function (method, url, sucess, fail, data, username, password) {
+
+            if (username === null || password === null) {
+                var req = {
+                    method: method,
+                    url: constants.rootURL + url,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: data
+                };
+            } else {
+                var req = {
+                    method: method,
+                    url: constants.rootURL + url,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': "Basic " + $base64.encode(username + ':' + password),
+                    },
+                    data: data,
+                    username: username,
+                    password: password
+                };
+            }
+            $http(req).then(sucess, fail);
+        };
+    }]);
