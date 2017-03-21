fe158280c60946262b08a73c7af6c40ef8fe682f
TaylorDoud92 <taylor.doud@edu.sait.ca>
2017-03-21 16:34:16 -0600

    Login changes and debug code

--- src/main/resources/static/js/controllers/login.js
+++ src/main/resources/static/js/controllers/login.js
@@ -19,7 +19,11 @@ angular.module('stars')
 				if(result.data.isAdmin){
 					isAdmin = true;
 					$cookieStore.put("isAdmin",isAdmin);
+					
+					//TODO
+					//CHANGE THIS PATH TO REDIRECT TO ADMIN VIEWS ONCE THEY'RE ARE DONE.
 					$location.path("records");
+					$window.location.reload();
 					//TODO
 					//$location.path("admin");
 				} else {
@@ -28,7 +32,8 @@ angular.module('stars')
 				}
             },
 			function (error) {
-                $scope.errMsg = "invalid username or password"
+            	console.log(error);
+                $scope.errMsg = "invalid username or password";
             },
             null,
             username,
@@ -40,6 +45,11 @@ angular.module('stars')
 	var username = $cookieStore.get("username");
 	var password = $cookieStore.get("password");
 	
-	if(username && password)
+	if(username && password){
+		console.log("auto logging in");
+		$cookieStore.remove("username");
+		$cookieStore.remove("password");
+		$cookieStore.remove("isAdmin");
 		selfReflect(username,password);
+	}
 }]);
