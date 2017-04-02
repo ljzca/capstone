273d73db2826a0d7434068f355b3e5bc7f5cca3a
ljzca <lijianzhao@lijianzhao.com>
2017-04-02 17:02:29 -0600

    Make the project auto-deployable
    - Separated development and production profiles;
    -- a separate database is no more needed under development profile;
    -- production database is as it used to be;
    - Auto create admin user;
    - Minor user bug fixing.

--- src/main/java/ca/sait/stars/domains/handlers/UserEventHandler.java
+++ src/main/java/ca/sait/stars/domains/handlers/UserEventHandler.java
@@ -10,35 +10,43 @@ import ca.sait.stars.domains.User;
 
 /**
  * This class is used to encrypt the user password
- * 
+ *
  * @author william
  *
  */
 @RepositoryEventHandler(User.class)
 @Service
 public class UserEventHandler {
-	
-	/**
-	 * handle password update
-	 * 
-	 * @param user
-	 */
-	@HandleBeforeSave
-	public void handleUserSave(User user) {
-		if (user.getPassword() == null || user.getPassword().isEmpty())
-			return;
-		
-		user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-	}
 
-	/**
-	 * handle user creation
-	 * 
-	 * @param user
-	 */
-	@HandleBeforeCreate
-	public void handleUserCreate(User user) {
+    /**
+     * handle password update
+     *
+     * @param user
+     */
+    @HandleBeforeSave
+    public void handleUserSave(User user) {
 
-		user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-	}
+        // no password will just use the original password
+        if (user.getPassword() == null || user.getPassword().isEmpty()) {
+            return;
+        }
+
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+    }
+
+    /**
+     * handle user creation
+     *
+     * @param user
+     */
+    @HandleBeforeCreate
+    public void handleUserCreate(User user) {
+
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+
+        // set default sex
+        if (user.getSex() == null) {
+            user.setSex(User.Sex.Unknown);
+        }
+    }
 }
--- src/main/java/ca/sait/stars/security/configurations/WebSecurityConfig.java
+++ src/main/java/ca/sait/stars/security/configurations/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package ca.sait.stars.security.configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -23,35 +24,58 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private UserDetailsService userDetailsService;
-	
-	@Autowired
-	private AuthenticationEntryPoint webAuthenticationEntryPoint;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-//			// To enable password encoder, this won't work
-//			.userDetailsService(userDetailsService)
-			.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and()
-			.httpBasic()
-			.authenticationEntryPoint(webAuthenticationEntryPoint)
-		.and()
-			.csrf()
-			.disable()
-		;
-	}
-	
-	@Autowired
-	public void configAuthentication(AuthenticationManagerBuilder auth)
-		throws Exception {
+    @Autowired
+    private AuthenticationEntryPoint webAuthenticationEntryPoint;
+    
+    @Value("${spring.profiles.active}")
+    private String environment;
 
-		auth
-			.userDetailsService(userDetailsService)
-			.passwordEncoder(new BCryptPasswordEncoder())
-		;
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        
+        http
+//            // To enable password encoder, this won't work
+//            .userDetailsService(userDetailsService)
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+            .httpBasic()
+            .authenticationEntryPoint(webAuthenticationEntryPoint)
+        .and()
+            .csrf()
+            .disable()
+        ;
+
+        /*
+         * to make H2 database console displayed
+         */
+        if("development".equals(environment))
+            http.headers().frameOptions().sameOrigin();
+    }
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth)
+        throws Exception {
+
+        auth
+            .userDetailsService(userDetailsService)
+            .passwordEncoder(new BCryptPasswordEncoder())
+        ;
+        
+        /*
+         * this is how to make inMemory users. Note: no "ROLE_" as a prefix of the role "ROLE_ADMIN", instead, just use "ADMIN"
+         */
+//        auth
+//            .inMemoryAuthentication()
+//                .withUser("admin")
+//                .password("password")
+//                .roles("ADMIN")
+//            .and()
+//                .withUser("user")
+//                .password("password")
+//        ;
+    }
 }
