895cf7289509141abe1999e6009b9c056da9bc8f
Jordan Pickett <jordan.j.pickett@gmail.com>
2017-04-08 15:08:02 -0600

    Got the object moving in the frame.

--- src/main/resources/static/js/factory/OBJmodel.js
+++ src/main/resources/static/js/factory/OBJmodel.js
@@ -1,45 +1,84 @@
-var camera, scene, renderer, mesh, loader;
+var container;
+			var camera, scene, renderer, myObj;
+			var mouseX = 0, mouseY = 0;
+			var windowHalfX = window.innerWidth / 2;
+			var windowHalfY = window.innerHeight / 2;
+			init();
+			animate();
+			function init() {
+				container = document.createElement( 'div' );
+				document.body.appendChild( container );
+				camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 1, 2000 );
+				camera.position.z = 250;
+				// scene
+				scene = new THREE.Scene();
+				var ambient = new THREE.AmbientLight( 0x101030 );
+				scene.add( ambient );
+				var directionalLight = new THREE.DirectionalLight( 0xffeedd );
+				directionalLight.position.set( 0, 0, 1 );
+				scene.add( directionalLight );
+				
+				// texture
+				var manager = new THREE.LoadingManager();
+				manager.onProgress = function ( item, loaded, total ) {
+					console.log( item, loaded, total );
+				};
+				var texture = new THREE.Texture();
+				var onProgress = function ( xhr ) {
+					if ( xhr.lengthComputable ) {
+						var percentComplete = xhr.loaded / xhr.total * 100;
+						console.log( Math.round(percentComplete, 2) + '% downloaded' );
+					}
+				};
+				var onError = function ( xhr ) {
+				};
+				var loader = new THREE.ImageLoader( manager );
+				loader.load( 'obj/SUIT.jpg', function ( image ) {
+					texture.image = image;
+					texture.needsUpdate = true;
+				} );
+				// model
+				var loader = new THREE.OBJLoader( manager );
+				loader.load( 'obj/wingsuit3.obj', function ( object ) {
+					myObj = object;
+					object.traverse( function ( child ) {
+						if ( child instanceof THREE.Mesh ) {
+							child.material.map = texture;
+						}
+					} );
+					
+					object.rotation.x = Math.PI/2 ;
+					object.rotation.z = Math.PI;					
+					scene.add( object );
+				}, onProgress, onError );
+				//
 
-init();
-animate();
-
-function init() {
-
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
-
-    scene = new THREE.Scene();
-
-    loader = new THREE.JSONLoader();
-
-    loader.load( "scripts/model.js", function( geometry ) {
-        mesh = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial() );
-        mesh.scale.set( 10, 10, 10 );
-        mesh.position.y = 150;
-        mesh.position.x = 0;
-    } );
-
-    scene.add( mesh );
-
-    var ambientLight = new THREE.AmbientLight(0x555555);
-    scene.add(ambientLight);
-
-    var directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.set(1, 1, 1).normalize();
-    scene.add(directionalLight);
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-    document.body.appendChild( renderer.domElement );
-
-}
-
-function animate() {
-
-    requestAnimationFrame( animate );
-
-    mesh.rotation.x += 0.05;
-
-    renderer.render( scene, camera );
-}
+				renderer = new THREE.WebGLRenderer();
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth/4, window.innerHeight/4);
+				container.appendChild( renderer.domElement );
+				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+				//
+				window.addEventListener( 'resize', onWindowResize, false );
+			}
+			function onWindowResize() {
+				windowHalfX = window.innerWidth / 2;
+				windowHalfY = window.innerHeight / 2;
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+				renderer.setSize( window.innerWidth, window.innerHeight );
+			}
+			function onDocumentMouseMove( event ) {
+				mouseX = ( event.clientX - windowHalfX ) / 2;
+				mouseY = ( event.clientY - windowHalfY ) / 2;
+			}
+			//
+			function animate() {
+				requestAnimationFrame( animate );
+				render();
+			}
+			function render() {
+//				myObj.rotation.x += 0.01;
+				camera.lookAt( scene.position );
+				renderer.render( scene, camera );
+			}
