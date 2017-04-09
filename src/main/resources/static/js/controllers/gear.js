3d9886c8d9c64be015bcb849ca5094ec2b5fa4a5
552494 <552494@ELF5IT212239.ACDM.DS.SAIT.CA>
2017-04-09 15:48:02 -0600

    Updated Gear pages

--- src/main/resources/static/js/controllers/gear.js
+++ src/main/resources/static/js/controllers/gear.js
@@ -1,56 +1,34 @@
 angular.module('stars')
 
-	.controller('gearCtrl',["$scope","$cookieStore","$location","$window","sendRequest", function ($scope, $cookieStore, $location, $window, sendRequest) {
+    .controller('gearCtrl', ["$scope", "$cookieStore", "$location", "$window", "sendRequest", function ($scope, $cookieStore, $location, $window, sendRequest) {
 
-		setNavBar();
-		
-	    //Populates Brand drop down.
-		var getBrand = function () {
-			sendRequest.send(
-				'GET',
-				'brands',
-				function (result) {
-					$scope.brands = result.data._embedded.brands;
-				},
-				function (error) {
-					$scope.notice = "Unable to retrieve companies.";
-				}
-			);
-		};
+        setNavBar();
 
-		//Populates Models drop down.
-		$scope.getModels = function() {
-			sendRequest.send(
-			'GET',
-			'brands/'+$scope.selectedBrand.id+'/models',
-			function (result)
-			{
-				console.log(result.data.models);
-				$scope.models = [];
-				for(var i=0;i<result.data._embedded.models.length;i++){
-				    var resultString = result.data._embedded.models[i]._links.model.href;
-				    $scope.models.push(decodeURIComponent(resultString.substring(resultString.indexOf("&")+1)));
-                }
-			},
-            function (error) {
-                console.log("getModels Failed");
-            }
-			)
-		};
-
-
-		$scope.getGearModels = function() {
+        //Populates Brand drop down.
+        var getBrand = function () {
             sendRequest.send(
                 'GET',
-                'brands/'+$scope.gear.brand+'/models',
-                function (result)
-                {
+                'brands',
+                function (result) {
+                    $scope.brands = result.data._embedded.brands;
+                },
+                function (error) {
+                    $scope.notice = "Unable to retrieve companies.";
+                }
+            );
+        };
+
+        //Populates Models drop down.
+        $scope.getModels = function () {
+            sendRequest.send(
+                'GET',
+                'brands/' + $scope.selectedBrand.id + '/models',
+                function (result) {
                     console.log(result.data.models);
                     $scope.models = [];
-                    for(var i=0;i<result.data._embedded.models.length;i++){
+                    for (var i = 0; i < result.data._embedded.models.length; i++) {
                         var resultString = result.data._embedded.models[i]._links.model.href;
-                        console.log(resultString);
-                        $scope.models.push(decodeURIComponent(resultString.substring(resultString.indexOf("&")+1)));
+                        $scope.models.push(decodeURIComponent(resultString.substring(resultString.indexOf("&") + 1)));
                     }
                 },
                 function (error) {
@@ -59,54 +37,82 @@ angular.module('stars')
             )
         };
 
-		//Retrieves User gear
-		var getGear = function () {
-			sendRequest.send(
-				'GET',
-				'users/' + $cookieStore.get('username') + '/gears',
-				function (result) {
-				    var gearListArray = [];
-				    for(var i=0;i<result.data._embedded.gears.length;i++){
-				        var resultString = result.data._embedded.gears[i]._links.gear.href;
-				        resultString = resultString.substring(33);
-				        var gearString = resultString.split("&");
-				        var gearObj = {
-				            brand: gearString[0],
+
+        $scope.getGearModels = function () {
+            sendRequest.send(
+                'GET',
+                'brands/' + $scope.gear.brand + '/models',
+                function (result) {
+                    console.log(result.data.models);
+                    $scope.models = [];
+                    for (var i = 0; i < result.data._embedded.models.length; i++) {
+                        var resultString = result.data._embedded.models[i]._links.model.href;
+                        console.log(resultString);
+                        $scope.models.push(decodeURIComponent(resultString.substring(resultString.indexOf("&") + 1)));
+                    }
+                },
+                function (error) {
+                    console.log("getModels Failed");
+                }
+            )
+        };
+
+        //Retrieves User gear
+        var getGear = function () {
+            sendRequest.send(
+                'GET',
+                'users/' + $cookieStore.get('username') + '/gears',
+                function (result) {
+                    var gearListArray = [];
+                    for (var i = 0; i < result.data._embedded.gears.length; i++) {
+                        var resultString = result.data._embedded.gears[i]._links.gear.href;
+                        resultString = decodeURIComponent(resultString.substring(33));
+                        var gearString = resultString.split("&");
+                        var gearObj = {
+                            brand: gearString[0],
                             model: gearString[1],
                             description: result.data._embedded.gears[i].description
                         };
-				        gearListArray.push(gearObj);
+                        gearListArray.push(gearObj);
                     }
                     console.log(gearListArray);
                     $scope.gearList = gearListArray;
-				},
-				function (error) {
-					// $scope.errMsg = "There was an error loading gear information";
-					console.log(error);
-				},
-				null,
-				$cookieStore.get('username'),
-				$cookieStore.get('password')
-			)
-		};
-
-		getGear();
-		getBrand();
-
-		$scope.addGear = function () {
-		    sendRequest.send(
-		        'POST',
-                'gears',
-                function(result){
-                    console.log("ITS GOOD!")
                 },
-                function(error){
-                    console.log("ITS BAD!")
+                function (error) {
+
+                    // $scope.errMsg = "There was an error loading gear information";
+                    console.log(error);
+                },
+                null,
+                $cookieStore.get('username'),
+                $cookieStore.get('password')
+            )
+        };
+
+        getGear();
+        getBrand();
+
+        $scope.addGear = function () {
+            sendRequest.send(
+                'POST',
+                'gears',
+                function (result) {
+                    console.log("ITS GOOD!");
+                    getGear();
+                },
+                function (error) {
+                    if (error.status === 409) {
+                        $scope.notice = "You cannot create duplicate pieces of gear!"
+                    } else {
+                        $scope.notice = "There was an error creating your gear!"
+                    }
                 },
                 {
-                    "id":{ "name":$scope.selectedBrand.id, "type":$scope.selectedModel, "owner":$cookieStore.get("username")},
-                    "model": "http://localhost:8080/rest/models/"+$scope.selectedBrand.id + "&" + $scope.selectedModel,
-                    "owner": "http://localhost:8080/rest/users/"+$cookieStore.get("username"),
+                    "id": {
+                        "name": $scope.selectedBrand.id,
+                        "type": $scope.selectedModel,
+                        "owner": $cookieStore.get("username")
+                    },
                     "description": $scope.selectedDescription
                 },
                 $cookieStore.get("username"),
@@ -114,41 +120,46 @@ angular.module('stars')
             )
         };
 
-		$scope.deleteGear = function () {
-          sendRequest.send(
-              'DELETE',
-              'gears',
-              function (result) {
+        $scope.deleteGear = function (brand, model) {
 
-              },
-              function (error){
-
-              },
-              null,
-              $cookieStore.get("username"),
-              $cookieStore.get("password")
-          )
+            sendRequest.send(
+                'DELETE',
+                'gears/' + encodeURIComponent(brand) + "&" + encodeURIComponent(model) + "&" + encodeURIComponent($cookieStore.get("username"))+"&"+"0",
+                function (result) {
+                    console.log("DELETED");
+                    getGear();
+                },
+                function (error) {
+                    console.log("ERROR DELETING")
+                },
+                null,
+                $cookieStore.get("username"),
+                $cookieStore.get("password")
+            )
         };
 
+        $scope.editGear = function (brand,model,description) {
 
-		$scope.deleteCompany = function (company) {
-			sendRequest.send(
-				'DELETE',
-				'company/' + company,
-				function (result) {
-					getCompanies();
-					$scope.notice = "You have successfully deleted" + company;
-				},
-				function (error) {
-					if (error.status === 406) {
-						$scope.notice = "You can't delete yourself";
-					} else {
-						$scope.notice = "An error has occured"
-					}
-				},
-				null,
-				$cookieStore.get("username"),
-				$cookieStore.get("password")
-			)
-		};
-	}]);
+            sendRequest.send(
+                'PUT',
+                'gears/'+encodeURIComponent(brand)+"&"+encodeURIComponent(model)+"&"+encodeURIComponent($cookieStore.get("username"))+"&"+"0",
+                function (result) {
+                    console.log("ITS GOOD")
+                    getGear();
+                },
+                function (error) {
+                    console.log("ERROR EDITING")
+                },
+                {
+                    "id":{
+                        "name": brand,
+                        "type": model,
+                        "owner": $cookieStore.get("username")
+                    },
+                    "description": description
+                },
+                $cookieStore.get("username"),
+                $cookieStore.get("password")
+            )
+        };
+    }]);
